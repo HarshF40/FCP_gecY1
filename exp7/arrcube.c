a56f7f07d18a9bfc81e8c538bcf7431841707fa1
HarshF40 <harshgaonkar57@gmail.com>
2024-10-11 13:00:06 +0530

    exp 7 complete

--- exp7/arrcube.c
+++ exp7/arrcube.c
@@ -1,6 +1,23 @@
 #include<stdio.h>
+#include<math.h>
+
+void cube(int (&arr)[], int size){
+  for(int i=0;i<size;i++){
+   arr[i] = pow(arr[i],3); 
+  }
+}
 
 int main(){
   int arr[10] = {1,2,3,4,5,6,7,8,9,0};
+      printf("\nArray before cubing: ");
+      for(int arr : arr){
+      printf("%d ",arr);
+      }
+  cube(arr,10);
+      printf("\nCubed Array: ");
+  for(int arr : arr){
+    printf("%d ",arr);
+  }
+      printf("\n");
   return 0;
 }
